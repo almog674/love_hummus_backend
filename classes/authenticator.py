d424af92c0c0a3281da6b3ec124b806d7ed7299e
almog674 <almogmaimon674@gmail.com>
2022-06-13 17:04:59 +0300

    Add functions to validate the name of the user.

--- classes/authenticator.py
+++ classes/authenticator.py
@@ -8,6 +8,10 @@ Date: 13/06/2022
 
 from models.userModel import UserModel
 from utilities.outputMessages import SuccessMessages
+from classes.database_query import DatabaseQuery
+from classes.db_adder import DatabaseAdder
+from utilities.outputMessages import ErrorMessages
+import pymongo
 
 
 class Authenticator:
@@ -17,21 +21,47 @@ class Authenticator:
     """
 
     @staticmethod
-    def signup(user_credentials: dict):
+    def signup(user_credentials: dict, users_collection: pymongo.collection):
         """
         :param user_credentials: The name & password of the user.
+        :param users_collection: The collection of users.
         :return: Message which tells if the operation was a success
         """
         # TODO - Add error handling
-        UserModel(name=user_credentials['name'], password=user_credentials['password'])
+        user_to_add = UserModel(name=user_credentials['name'], password=user_credentials['password'])
+        DatabaseAdder.add_item(user_to_add.dict(), users_collection)
         return SuccessMessages.USER_CREATED
 
-
-    @staticmethod
-    def login(user_credentials: dict):
+    def login(self, user_credentials: dict, users_collection: pymongo.collection):
         """
         :param user_credentials: The name & password of the user.
+        :param users_collection: The collection of users.
         :return: Message which tells if the operation was a success
         """
-        # Uses the DB_query
-        pass
+        # Check user exist
+        user = self.get_user_by_name(users_collection)
+        if self.validate_user(user):
+            if user['password'] != user_credentials['passowrd']:
+                pass
+
+        return user
+
+    @staticmethod
+    def get_user_by_name(name: str, users_collection: pymongo.collection):
+        """
+            :param name: The name of the user we want to get.
+            :param users_collection: The collection of users.
+        """
+        user = DatabaseQuery.execute_query({"name": name}, users_collection)
+        return user
+
+    @staticmethod
+    def validate_user(user_to_check: pymongo.cursor):
+        """
+            :param user_to_check: The user we want to validate.
+        """
+        is_user_valid = True
+        if not user_to_check:
+            print(ErrorMessages.USER_DOES_NOT_EXIST)
+            is_user_valid = False
+        return is_user_valid
