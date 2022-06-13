7700b81350f38e0dfd738cffbe109b64727b1e85
Jake Klein <jakeklein91@gmail.com>
2022-06-13 19:00:08 +0300

    Add consts

--- routers/hummus.py
+++ routers/hummus.py
@@ -8,6 +8,7 @@ from fastapi import APIRouter, status
 from uuid import UUID
 from models import humusia_model, filter_model
 from server import DB_MANAGER
+from utilities.consts import HummusRouter, UserReturnPrompts
 
 HUMMUS_ROUTES = APIRouter()
 
@@ -18,7 +19,7 @@ async def add_hummusia(hummusia: humusia_model.HumusiaModel):
     Adds the hummusia received from the body of the request to the database.
     """
     DB_MANAGER._db_adder.add_item(dict(hummusia), DB_MANAGER.humusiot_collection)
-    return "Added"
+    return UserReturnPrompts.add_prompt
 
 
 @HUMMUS_ROUTES.put("/hummusiot/{hummusia_id}/{rating_given}")
@@ -27,15 +28,18 @@ async def add_rating_to_hummusia(hummusia_id: UUID, rating_given: int):
     Updates the hummusia fields according to the rating given
     as a path parameter.
     """
-    hummusia = DB_MANAGER._db_query.execute_query({"id": hummusia_id})[0]
-    DB_MANAGER._db_updater.update_item(hummusia_id, {"rating_sum": hummusia["rating_sum"] + rating_given,
-                                                     "number_of_ratings": hummusia["number_of_ratings"] + 1})
-    return "updated"
+    hummusia = DB_MANAGER._db_query.execute_query({"id": hummusia_id})[HummusRouter.ONE_ID_PER_HUMMUSIA]
+    DB_MANAGER._db_updater.update_item(hummusia_id,
+                                       {"rating_sum": hummusia["rating_sum"] + rating_given,
+                                        "number_of_ratings": hummusia[
+                                         "number_of_ratings"] + HummusRouter.INCREMENT_NUMBER_OF_RATINGS})
+
+    return UserReturnPrompts.update_prompt
 
 
 @HUMMUS_ROUTES.get("/hummusiot/")
 async def get_hummusia_by_filter(db_filter: filter_model):
     """
-    Sends the user all hummsiot matching the given filter.
+    Sends the user all hummusiot matching the given filter.
     """
     return DB_MANAGER._db_query.execute_query(db_filter.db_filter)
--- utilities/consts.py
+++ utilities/consts.py
@@ -12,3 +12,12 @@ class HummusRouter:
     """
     ONE_ID_PER_HUMMUSIA = 0
     INCREMENT_NUMBER_OF_RATINGS = 1
+
+
+class UserReturnPrompts:
+    """
+    Contains all prompts returned to the user after
+    his fullfiled request.
+    """
+    update_prompt = "Updated"
+    add_prompt = "Added"
