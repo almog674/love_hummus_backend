3cd755f290a186b360890411c058b2b3133bdd45
Jake Klein <jakeklein91@gmail.com>
2022-06-13 16:29:36 +0300

    Finish implementation of DataBaseQuery

--- classes/database_query.py
+++ classes/database_query.py
@@ -4,9 +4,18 @@ Name: database_query.py
 Author: Hanich 5
 Purpose: Contains the implementation of an object that queries the database.
 """
-import pymongo
+from pymongo import collection as mongo_collection
 from interfaces import database_excuting
 
 
-class DatabaseQuery(database_excuting):
-    pass
+class DatabaseQuery(database_excuting.IDbQuery):
+    """
+    Implementation of IDbQuery for mongoDB.
+    """
+    def execute_query(self, item: dict, collection: mongo_collection) -> list:
+        """
+        :param item: The query itself.
+        :param collection: The collection to query.
+        :return: Returns a list of all query results.
+        """
+        return collection.find(item)
