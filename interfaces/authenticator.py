cba1dd8627ea5906437fab4542fe4ba381e2378f
almog674 <almogmaimon674@gmail.com>
2022-06-13 15:04:28 +0300

    Add the authenticator class.

---
+++ interfaces/authenticator.py
@@ -0,0 +1,16 @@
+"""
+Name: authenticator.py
+Author: Hanich 08
+Purpose: This class handle the authentication process, it helps you log into your user
+or create new user.
+Date: 13/06/2022
+"""
+
+
+class Authenticator:
+    """
+    This class handle the authentication process, it helps you log into your user
+    or create new user.
+    """
+    def __init__(self):
+        pass
