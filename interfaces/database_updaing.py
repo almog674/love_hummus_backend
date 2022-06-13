f85c815a54390d9822ae976af9915a35479ac06c
almog674 <almogmaimon674@gmail.com>
2022-06-13 16:21:36 +0300

    CHange the interface of the database updating to get also the 'item_id'.

--- interfaces/database_updaing.py
+++ interfaces/database_updaing.py
@@ -3,12 +3,14 @@ Purpose: update item interface
 Author: Hanich 02 (13.6.22)
 """
 from pymongo import collection as mongo_collection
+from uuid import UUID
 
 
 class DbUpdater:
     """
     interface of update item in collection
     """
+
     @staticmethod
-    def update_item(new_values: dict, collection: mongo_collection):
+    def update_item(item_id: UUID, new_values: dict, collection: mongo_collection):
         raise NotImplementedError
