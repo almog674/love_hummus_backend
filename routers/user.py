6eaa9edae237a19003dacf19e30a4260d768ea86
Jake Klein <jakeklein91@gmail.com>
2022-06-13 19:09:38 +0300

    Add model to user router

--- routers/user.py
+++ routers/user.py
@@ -5,15 +5,17 @@ Author: Hanich 5
 Purpose: Contain all user related routers.
 """
 from fastapi import APIRouter
+from models import userModel
+from server import DB_MANAGER
 
 USER_ROUTES = APIRouter()
 
 
 @USER_ROUTES.post("/user/login")
-async def login():
+async def login(user: userModel.UserModel):
     pass
 
 
 @USER_ROUTES.post("/user/signup")
-async def login():
+async def sign_up(user: userModel.UserModel):
     pass
