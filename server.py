a476304c551c0c5ea6170f571805c2c31be2ab63
Jake Klein <jakeklein91@gmail.com>
2022-06-13 14:08:36 +0300

    Add general template to server.py

---
+++ server.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python
+"""
+Name: server.py
+Author: Hanich 5
+Purpose: Main FastAPI entrypoint.
+"""
+from fastapi import FastAPI
+
+APP = FastAPI()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
