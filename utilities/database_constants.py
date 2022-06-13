08a3dacc2f8f79c7eadf54366183f64ed0b99cc4
almog674 <almogmaimon674@gmail.com>
2022-06-13 14:22:02 +0300

    Add the class 'DatabaseConstants' for holding the database consts

--- utilities/database_constants.py
+++ utilities/database_constants.py
@@ -6,3 +6,10 @@ Date: 13/06/2022
 """
 
 
+class DatabaseConstants:
+    """
+    Constants for the connection to the database
+    """
+    DATABASE_NAME = "humusiat_ron_ubanav"
+    USERS_COLLECTION_NAME = "users"
+    HUMUSIOT_COLLECTION_NAME = "humusiot"
