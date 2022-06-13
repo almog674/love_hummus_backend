0d3f203f04f35a135171a3626ca62040b1705b9c
Jake Klein <jakeklein91@gmail.com>
2022-06-13 16:20:50 +0300

    Add template for database query

---
+++ classes/database_query.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Name: database_query.py
+Author: Hanich 5
+Purpose: Contains the implementation of an object that queries the database.
+"""
+import pymongo
+from interfaces import database_excuting
+
+
+class DatabaseQuery(database_excuting):
+    pass
