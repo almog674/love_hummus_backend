ac3c5b032ba97c1580b78ad0b2d9757b2237c6d2
almog674 <almogmaimon674@gmail.com>
2022-06-13 15:32:39 +0300

    Build the signup function

--- classes/authenticator.py
+++ classes/authenticator.py
@@ -6,11 +6,24 @@ or create new user.
 Date: 13/06/2022
 """
 
+from models.userModel import UserModel
+from utilities.outputMessages import SuccessMessages
+
 
 class Authenticator:
     """
     This class handle the authentication process, it helps you log into your user
     or create new user.
     """
-    def __init__(self):
-        pass
+
+    @staticmethod
+    def signup(user_credentials: dict):
+        """
+        :param user_credentials: The name & password of the user.
+        :return:
+        """
+        # TODO - Add error handling
+        UserModel(name=user_credentials['name'], password=user_credentials['password'])
+        return SuccessMessages.USER_CREATED
+
+
