056edc6fdaa0afd344dd31abb7c7d4aa807a37a5
Yuval <yuvalaviv8@gmail.com>
2022-06-13 15:18:14 +0300

    Upload dict cont

---
+++ utilities/database_functions.py
@@ -0,0 +1,13 @@
+"""
+Name: database_functions.py
+Author: Hanich 02
+Purpose: available functions in dictionary
+Date: 13/06/2022
+"""
+
+
+class DBFunctions:
+    """
+    dictionary of the available functions
+    """
+    FUNCTIONS_DICT = {"update": print('update'), "add": print('add'), "execute": print('execute')}
